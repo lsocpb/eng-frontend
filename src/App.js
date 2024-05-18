f447a72ddd4b8ee3418f9b6365cfc24a99e3d442
lsocpb <lukasssonik@gmail.com>
2024-05-18 22:15:17 +0200

    +track user session +sign in visual fixes +unauth, auth navbar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Login from './Components/LoginForm/Login';
+import ProtectedPage from './Protected';
+import RoleNavbar from './Components/Navbar/RoleNavbar';
+import Register from "./Components/Register/Register";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <Router>
+            <RoleNavbar/>
+            <Routes>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/protected" element={<ProtectedPage/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
