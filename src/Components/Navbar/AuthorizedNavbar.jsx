35a489251f4ae4fd64e4c4f77c83b8211a06eba4
lsocpb <lukasssonik@gmail.com>
2024-07-25 22:55:01 +0200

    navbar visual fixes

--- src/Components/Navbar/AuthorizedNavbar.jsx
+++ src/Components/Navbar/AuthorizedNavbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
     MDBNavbar,
     MDBContainer,
@@ -33,7 +33,7 @@ export default function App() {
     return (
         <>
             <MDBNavbar sticky bgColor='white' expand='lg'>
-                <MDBContainer fluid>
+                <MDBContainer>
                     <MDBNavbarBrand href='/home' className='mx-3 d-flex'>
                         <img src={process.env.PUBLIC_URL + '/chairfair.png'} style={{width: '150px', height: '60px'}} alt="CharFair logo"/>
                     </MDBNavbarBrand>
@@ -47,52 +47,44 @@ export default function App() {
                     >
                         <MDBIcon icon='bars' fas/>
                     </MDBNavbarToggler>
-                    <MDBCollapse navbar open={openNavNoTogglerSecond}>
-                        <MDBNavbarNav className='mb-2 px-4 mb-lg-0 align-items-center'>
-                            <MDBNavbarItem className="flex-grow-1 mx-5 mt-1">
-                                <form onSubmit={searchProducts}>
-                                    <MDBInputGroup>
-                                        <input
-                                            type='search'
-                                            className='form-control rounded-pill'
-                                            placeholder='Search for products...'
-                                            aria-label='Search'
-                                            value={searchText}
-                                            onChange={(e) => setSearchText(e.target.value)}
-                                        />
-                                        <MDBBtn color='danger' rippleColor='dark' className='rounded-pill ms-2' type='submit'>
-                                            <MDBIcon fas icon='search' />
-                                        </MDBBtn>
-                                    </MDBInputGroup>
-                                </form>
-                            </MDBNavbarItem>
-                            <MDBNavbarItem>
-                                <MDBNavbarLink active aria-current='page' href='/home' className='text-dark'>
-                                    <MDBIcon fas icon="home" className="me-2 mx-4"/> Home
-                                </MDBNavbarLink>
-                            </MDBNavbarItem>
-                            <MDBNavbarItem>
-                                <MDBNavbarLink active aria-current='page' href='/profile' className='text-dark'>
-                                    <MDBIcon fas icon="user" className="me-2 mx-4"/> Profile
-                                </MDBNavbarLink>
-                            </MDBNavbarItem>
-                            <MDBNavbarItem>
-                                <MDBNavbarLink active aria-current='page' href='/admin/category/all' className='text-dark'>
-                                    <MDBIcon fas icon="list" className="me-2 mx-4" /> Causes
-                                </MDBNavbarLink>
-                            </MDBNavbarItem>
-                            <MDBNavbarItem>
-                                <MDBNavbarLink active aria-current='page' href='/product/add' className='text-dark'>
-                                    <MDBIcon fas icon="hand-holding-heart" className="me-2 mx-4" /> Donate
-                                </MDBNavbarLink>
-                            </MDBNavbarItem>
-                            <MDBNavbarItem>
-                                <MDBBtn color='danger' className="rounded-pill mx-5" onClick={userLogout}>
-                                    <MDBIcon fas icon="sign-out-alt" className="me-2" /> Logout
-                                </MDBBtn>
-                            </MDBNavbarItem>
-                        </MDBNavbarNav>
-                    </MDBCollapse>
+                        <MDBCollapse navbar open={openNavNoTogglerSecond}>
+                            <MDBNavbarNav className='mb-2 px-4 mb-lg-0 align-items-center'>
+                                <MDBNavbarItem className="flex-grow-1 mx-5 mt-1">
+                                    <form onSubmit={searchProducts} className="search-form">
+                                        <MDBInputGroup>
+                                            <input
+                                                type='search'
+                                                className='form-control rounded-pill'
+                                                placeholder='Search for products...'
+                                                aria-label='Search'
+                                                value={searchText}
+                                                onChange={(e) => setSearchText(e.target.value)}
+                                            />
+                                            <MDBBtn color='danger' rippleColor='dark' className='rounded-pill ms-2'
+                                                    type='submit'>
+                                                <MDBIcon fas icon='search'/>
+                                            </MDBBtn>
+                                        </MDBInputGroup>
+                                    </form>
+                                </MDBNavbarItem>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink active aria-current='page' href='/home' className='text-dark'>
+                                        <MDBIcon fas icon="home" size={"lg"} className="me-2 mx-4"/>
+                                    </MDBNavbarLink>
+                                </MDBNavbarItem>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink active aria-current='page' href='/profile' className='text-dark'>
+                                        <MDBIcon fas icon="user" size={"lg"} className="me-2 mx-4"/>
+                                    </MDBNavbarLink>
+                                </MDBNavbarItem>
+                                <MDBNavbarItem>
+                                    <MDBBtn rounded pill aria-current='page' href='/product/add'
+                                            className='mx-5 text-white btn-danger'>
+                                        <MDBIcon fas icon="hand-holding-heart" className="me-2"/> Donate
+                                    </MDBBtn>
+                                </MDBNavbarItem>
+                            </MDBNavbarNav>
+                        </MDBCollapse>
                 </MDBContainer>
             </MDBNavbar>
         </>
